dd7267d110aab58fb41b159fa6fea8aecce1fb17
kevin <kevin.riedl.privat@gmail.com>
2018-06-24 13:59:00 +0200

    added actions

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,17 +1,27 @@
 import logging
+from threading import Thread
+
+import os
+
+import sys
 import telegram
 from telegram.error import NetworkError, Unauthorized
-from time import sleep
+from time import sleep, strftime, gmtime
+
+from telegram.ext import Updater
+
 from PyChatbot import PyChatbot
 
 update_id = None
 TOKEN = "440393301:AAHVa5olsKOY66f6NM-lEg1e74pY3jngfCw"
+bot = telegram.Bot(TOKEN)
+updater = Updater(TOKEN)
+
 
 def main():
     global update_id
-    bot = telegram.Bot(TOKEN)
 
-    #get first pending update id, this is so we can skip over it in case
+    # get first pending update id, this is so we can skip over it in case
     # we get an unauthorized exception
     try:
         update_id = bot.get_updates()[0].update_id
@@ -24,19 +34,40 @@ def main():
         try:
             echo(bot)
         except Unauthorized:
-            #The user has removed or blocked the bot
+            # The user has removed or blocked the bot
             update_id += 1
+
+
+# Thread(target=stop_and_restart).start()
+def stop_and_restart():
+    updater.stop()
+    os.execl(sys.executable, sys.executable, *sys.argv)
+
 
 def echo(bot):
     global update_id
-    #Requests updates after the last update id
-    for update in bot.get_updates(offset=update_id, timeout=20000):
-        update_id = update.update_id +1
+    chat_id = bot.get_updates()[-1].message.chat_id
 
-        if update.message: #your bot can receive updates without messages
-            #reply to msg
-            update.message.reply_text(PyChatbot.getAnswer(update.message.text))
+    # Requests updates after the last update id
+    for update in bot.get_updates(offset=update_id, timeout=20000):
+        update_id = update.update_id + 1
+
+        if update.message:  # your bot can receive updates without messages
+            # reply to msg
+            text = update.message.text
+            if any(x in text for x in ["pic", "image"]):
+                # Send action
+                bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.UPLOAD_PHOTO)
+                sleep(3)
+                # time attached to avoid caching so each time a new image is sent (so url is unique every time)
+                bot.send_photo(chat_id=chat_id,
+                               photo='https://picsum.photos/400?random' + strftime("%Y-%m-%d_%H-%M-%S", gmtime()))
+            else:
+                # Send action
+                bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
+                sleep(2)
+                # answer as text message
+                update.message.reply_text(PyChatbot.getAnswer(text))
 
 if __name__ == '__main__':
     main()
-
