61f4a3ebccb0353c26efe86845e36bd579e34fcf
kevin <kevin.riedl.privat@gmail.com>
2018-06-27 23:42:05 +0200

    added machine learning framework

--- KEVIN/Answers/AnswerQuestion.py
+++ KEVIN/Answers/AnswerQuestion.py
@@ -1,8 +1,18 @@
 from Answers._Answer import Answer
+from chatterbot import ChatBot
+
+chatbot = ChatBot(
+    'KevKevin',
+    trainer='chatterbot.trainers.ChatterBotCorpusTrainer'
+)
+
+#Train based on english corpus
+chatbot.train("chatterbot.corpus.english")
 
 
 class AnswerQuestion(Answer):
     @staticmethod
     def getAnswer(userInput):
         # TODO: implement regex etc.
-        return "Sorry, I didn't understand your question."
+        #return "Sorry, I didn't understand your question."
+        return chatbot.get_response(userInput)
