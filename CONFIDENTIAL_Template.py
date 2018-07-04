b0495cd67a119834d094c77a854ed4a723389c5f
kevin <kevin.riedl.privat@gmail.com>
2018-07-04 14:35:58 +0200

    added template

---
+++ CONFIDENTIAL_Template.py
@@ -0,0 +1,11 @@
+# Only one authorized user as chat_id
+AUTHORIZED_USER = 0 # Your telegram chat id here (so the chatbot only interacts with you)
+
+# Telegram Token
+TOKEN = "YOUR_TELEGRAM_TOKEN_HERE"
+
+# Instagram Login Credentials
+INSTAGRAM_API = {
+    "USERNAME":"YOUR_USERNAME_HERE",
+    "PASSWORD":"YOUR_PASSWORD_HERE"
+}
