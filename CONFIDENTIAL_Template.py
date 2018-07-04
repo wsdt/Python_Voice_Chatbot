17b6a17d8ced28c0765098738782fccbdd50bfd6
kevin <kevin.riedl.privat@gmail.com>
2018-07-04 14:39:10 +0200

    added comment

--- CONFIDENTIAL_Template.py
+++ CONFIDENTIAL_Template.py
@@ -1,3 +1,7 @@
+""" Change all params in this file and rename the file itself after all that to CONFIDENTIAL.py
+The modified file is also in the .gitignore, so your data is secure :) """
+
+
 # Only one authorized user as chat_id
 AUTHORIZED_USER = 0 # Your telegram chat id here (so the chatbot only interacts with you)
 
