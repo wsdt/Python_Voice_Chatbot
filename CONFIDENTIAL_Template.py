a9a631dcb51a2121035cf50a45163beaab0eff24
DDr.habil (FH) PHD Riedl Kevin, MSc <kevin.riedl.privat@gmail.com>
2018-07-08 22:18:11 +0200

    Update CONFIDENTIAL_Template.py

--- CONFIDENTIAL_Template.py
+++ CONFIDENTIAL_Template.py
@@ -2,13 +2,20 @@
 The modified file is also in the .gitignore, so your data is secure :) """
 
 
-# Only one authorized user as chat_id
+""" ++++++++++++++++++++ Only one authorized user as chat_id +++++++++++++++++++++++
+To get your chat_id just start the bot with a valid telegram token and look at your
+console output. Just grab your id and put it here :) """
 AUTHORIZED_USER = 0 # Your telegram chat id here (so the chatbot only interacts with you)
 
-# Telegram Token
+""" +++++++++++++++++++++ Telegram Token +++++++++++++++++++++++++++++++++++++++++++
+Every Telegrambot needs a token. You get it from the superior bot called 'Botfather'. 
+For more information please Google it, bc. there is certainly a better documentation. 
+"""
 TOKEN = "YOUR_TELEGRAM_TOKEN_HERE"
 
-# Instagram Login Credentials
+""" ++++++++++++++++++++++ Instagram Login Credentials ++++++++++++++++++++++++++++++ 
+If you want to use the Instagram Features just add your username and password below. 
+"""
 INSTAGRAM_API = {
     "USERNAME":"YOUR_USERNAME_HERE",
     "PASSWORD":"YOUR_PASSWORD_HERE"
