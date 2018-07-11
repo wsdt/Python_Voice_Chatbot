59864637e8f224ab057d993fb85f39c9fb3d50ad
kevin <kevin.riedl.privat@gmail.com>
2018-07-11 19:28:22 +0200

    added live speech test

--- Answers/api/test_Voice.py
+++ Answers/api/test_Voice.py
@@ -4,12 +4,17 @@
 # Speech to Text and reverse
 # Testing file
 
-from pocketsphinx import AudioFile
+from pocketsphinx import LiveSpeech,AudioFile,get_model_path
 import urllib
 import os
 
 class test_Voice:
     LAST_VOICE_FILE = "user_voice_msg.ogg"
+
+    @staticmethod
+    def liveSpeech():
+        for phrase in LiveSpeech():
+            print(phrase)
 
     @staticmethod
     def downloadVoiceFile(update):
@@ -22,8 +27,17 @@ class test_Voice:
     def test(update):
         test_Voice.downloadVoiceFile(update)
 
+
+        model_path = get_model_path()
         config = {
-            'audio_file': os.path.join("files",test_Voice.LAST_VOICE_FILE)
+            'audio_file': os.path.join("files",test_Voice.LAST_VOICE_FILE),
+            'verbose': False,
+            'buffer_size': 2048,
+            'no_search': False,
+            'full_utt': False,
+            'hmm': os.path.join(model_path, 'en-us'),
+            'lm': os.path.join(model_path, 'en-us.lm.bin'),
+            'dict': os.path.join(model_path, 'cmudict-en-us.dict')
         }
 
         for phrase in AudioFile(**config):
@@ -31,3 +45,5 @@ class test_Voice:
 
 
 
+if __name__ == '__main__':
+    test_Voice.liveSpeech()
