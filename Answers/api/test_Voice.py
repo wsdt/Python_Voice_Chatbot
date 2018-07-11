885e86a0ff7437b1d153bbf0a113291e5cebbd21
kevin <kevin.riedl.privat@gmail.com>
2018-07-11 18:53:36 +0200

    added constatn

--- Answers/api/test_Voice.py
+++ Answers/api/test_Voice.py
@@ -9,20 +9,21 @@ import urllib
 import os
 
 class test_Voice:
+    LAST_VOICE_FILE = "user_voice_msg.ogg"
+
+    @staticmethod
+    def downloadVoiceFile(update):
+        filedata = urllib.request.urlopen(str(update.message.voice.get_file().file_path)).read()
+
+        with open('files/'+test_Voice.LAST_VOICE_FILE, 'wb') as f:
+            f.write(filedata)
 
     @staticmethod
     def test(update):
-        filedata = urllib.request.urlopen(
-            str(update.message.voice.get_file().file_path)
-        )
-
-        datatowrite = filedata.read()
-
-        with open('files/test.ogg', 'wb') as f:
-            f.write(datatowrite)
+        test_Voice.downloadVoiceFile(update)
 
         config = {
-            'audio_file': os.path.join("files","test.ogg")
+            'audio_file': os.path.join("files",test_Voice.LAST_VOICE_FILE)
         }
 
         for phrase in AudioFile(**config):
