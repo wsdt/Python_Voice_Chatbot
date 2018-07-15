806093f15688fc08adc822f2b20d3cfbbcbcd5fe
kevin <kevin.riedl.privat@gmail.com>
2018-07-15 20:36:10 +0200

    added first db logic

--- configure.py
+++ configure.py
@@ -1,4 +1,5 @@
 import os
+from mgr.mgr_db import doWithDb
 
 # TODO: Import the configuration needs to be done AFTER the smart-learning! So maybe place that also here (otherwise learning is never done)
 
@@ -14,8 +15,15 @@ def config_confidential():
         print("configure: CONFIDENTIAL.py found.\n"
               "If you have started this bot with that file before then I recommend that you rename your CONFIDENTIAL.py to CONFIDENTIAL_backup.py for a better performance.\n"
               "If you have not started this assistant with a valid CONFIDENTIAL.py before, then you did everything right :)")
-        # TODO: save to db
+        # save to db
+        doWithDb(save_confidential) #do not call function just give it to doWithDb
     else:
         print("configure: Have not found a CONFIDENTIAL.py. \n"+
               "If you have started this bot with that file before then everything should be fine :).\n"
               "If you have not configured your default settings, then the assistant might not work correctly.")
+
+
+# Only invoked when confidential.py exists
+def save_confidential(dbCursor):
+    print("configure: Saving confidential.py.")
+    #TODO: convert confidential py to db and create tables if they dont exist
--- mgr/mgr_db.py
+++ mgr/mgr_db.py
@@ -8,5 +8,5 @@ def doWithDb(whatToDo):
     db = sqlite3.connect('db')
     # Execute provided function with dbCursor
     whatToDo(db.cursor())
-    db.commit() #save changes
+    db.commit() # save changes
     db.close()
