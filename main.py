3541e147611708cbd523be4a1ffa98443a1a4cb6
kevin <kevin.riedl.privat@gmail.com>
2018-07-17 19:35:25 +0200

    added error msg for not executing setup.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
+import os, sys
+from global_constants import DB_NAME
+
+# Catch before other imports to avoid confusing error msgs
+if not os.path.exists(DB_NAME):
+    sys.exit("ERROR: No database detected. Please execute setup.py first!")
+
 from mgr.mgr_voices import liveSpeech
 from peewee import *
-from mgr.mgr_db import *
 
 
 
 def main():
     # Start Voice Recognition
     liveSpeech()
+
 
 if __name__ == '__main__':
     main()
